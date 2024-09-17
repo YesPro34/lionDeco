30612e46fb355da650abd85e064427f80c553bae
YesPro34 <echamkh786@gmail.com>
2024-09-17 19:48:02 +0100

    v1 project

--- main.js
+++ main.js
@@ -10,30 +10,13 @@ $(document).ready(function(){
     $('nav').removeClass('nav-toggle');
   });
 
-  $('.count').each(function() {
-    var $this = $(this),
-        countTo = $this.attr('data-count');
-    $({ countNum: $this.text()}).animate({
-      countNum: countTo
-    },
-    {
-      duration: 5000,
-      step: function() {
-        $this.text(Math.floor(this.countNum));
-      },
-      complete: function() {
-        $this.text(this.countNum + '+');
-      }
-    });
-  });
-
-  $('.project').magnificPopup({
-    delegate:'a',
-    type:'image',
-    gallery:{
-      enabled:true
-    }
-  });
+  // $('.project').magnificPopup({
+  //   delegate:'a',
+  //   type:'image',
+  //   gallery:{
+  //     enabled:true
+  //   }
+  // });
 
 });
 
